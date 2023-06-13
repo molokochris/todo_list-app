3dc4d1535a215f9b082d4c4aaa66c6b85b344336
molokochris <molokochrisp742@gmail.com>
2023-06-13 11:34:03 +0200

    initial

---
+++ src/Pages/Home.js
@@ -0,0 +1,29 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <nav className="head">
+        <Link to="/">
+          <img src="./logo.png" alt="" />
+        </Link>
+      </nav>
+      <div className="inner">
+        <div className="container-tab">
+          <div className="account-tab">
+            <Link className="login-tab" to="/login">Login</Link>
+            <Link className="register-tab" to="/register">
+              Register
+            </Link>
+          </div>
+        </div>
+      </div>
+      <footer>&copy;</footer>
+    </div>
+  );
+}
