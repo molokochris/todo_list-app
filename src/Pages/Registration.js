9d2dac5fa863a1044b25eeeff8c7f301903661ad
molokochris <molokochrisp742@gmail.com>
2023-06-13 13:54:46 +0200

    updated

--- src/Pages/Registration.js
+++ src/Pages/Registration.js
@@ -1,7 +1,15 @@
 export default function Registration() {
-    return (
-        <>
-            Registration
-        </>
-    );
+  return (
+    <div className="container">
+      <div className="form-container">
+        <input className="names" type="text" placeholder="First Names" />
+        <input className="names" type="text" placeholder="Surname" />
+        <br />
+        <input className="" type="email" placeholder="Email address" />
+        <br />
+        <input className="" type="password" placeholder="Password" />
+        <button>Create account</button>
+      </div>
+    </div>
+  );
 }
