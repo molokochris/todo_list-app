183f6afcbc3187eb7f0978416fa8cc7b21b36253
molokochris <molokochrisp742@gmail.com>
2023-06-19 16:07:18 +0200

    updated wit modal

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -20,23 +20,23 @@ export default function Dashboard() {
     updateCard([...cards, card]);
   };
 
+  const handleDelete = (e) => {};
+
   const handleTitleChange = (e) => {
     updateTitle(e.target.value);
-  }
+  };
   const handleSubChange = (e) => {
     updateSub(e.target.value);
-  }
+  };
   const handleTextChange = (e) => {
     updateInnerText(e.target.value);
-  }
+  };
   const handleKeyDown = (e) => {
-    
     if (e.key === "Enter") {
       note.push(cards);
-      console.log("enter pressed")
-     }
-  }
-      console.log(note)
+      console.log("enter pressed");
+    }
+  };
 
   return (
     <div className="dashboard-container">
@@ -51,7 +51,7 @@ export default function Dashboard() {
                 className="card bg-primary custom-card"
                 style={{ width: "30%", height: "20%", margin: "1%" }}
               >
-                <div className="delete-card">
+                <div className="delete-card" onClick={handleDelete}>
                   <img
                     src="/delete-card.svg"
                     style={{ width: "20px", height: "auto" }}
@@ -88,8 +88,45 @@ export default function Dashboard() {
             );
           })}
         </div>
-        <div className="add" onClick={handleClick}>
-          <span className="add-icon">+</span>
+        <button className="add" data-bs-toggle="modal" data-bs-target="#exampleModal">
+          {/* <span className="add-icon">+</span> */}launch
+        </button>
+        {/* <!-- Modal -- */}
+        <div
+          class="modal fade"
+          id="exampleModal"
+          tabindex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">
+                  Modal title
+                </h1>
+                <button
+                  type="button"
+                  class="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div class="modal-body">...</div>
+              <div class="modal-footer">
+                <button
+                  type="button"
+                  class="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                >
+                  Close
+                </button>
+                <button type="button" class="btn btn-primary">
+                  Save changes
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
