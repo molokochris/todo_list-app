e52992087099f113055d69530d1a97920cc85edf
molokochris <molokochrisp742@gmail.com>
2023-06-19 15:54:13 +0200

    updated cards

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,150 +1,43 @@
 import { useState } from "react";
-import Card from "./Card";
-
-// export default function Dashboard() {
-//   const [doneCards, setDoneCards] = useState([]);
-
-//   const [cards, setCards] = useState([
-//     {
-//       id: 1,
-//       title: "Title 1",
-//       body: "Body 1",
-//       color: "bg-primary",
-//       isChecked: false,
-//     },
-//   ]);
-
-//   const handleAddCard = () => {
-//     const newCard = {
-//       id: cards.length + 1,
-//       title: "New Title",
-//       body: "New Body",
-//       color: "bg-primary",
-//       isChecked: false,
-//     };
-//     setCards([...cards, newCard]);
-//   };
-
-//   const handleClick = (id) => {
-//     setCards(
-//       cards.map((card) => {
-//         if (card.id === id) {
-//           let newColor = "bg-danger";
-//           if (card.color === "bg-danger") {
-//             newColor = "bg-primary";
-//           } else if (card.color === "bg-primary") {
-//             newColor = "bg-success";
-//           } else if (card.color === "bg-success") {
-//             newColor = "bg-danger";
-//           }
-//           return {
-//             ...card,
-//             color: newColor,
-//           };
-//         }
-//         return card;
-//       })
-//     );
-//   };
-
-//   const handleCheckboxChange = (id) => {
-//     setCards((prevCards) => {
-//       const updatedCards = prevCards.map((card) => {
-//         if (card.id === id) {
-//           return {
-//             ...card,
-//             isChecked: !card.isChecked,
-//           };
-//         }
-//         return card;
-//       });
-//       const checkedCard = updatedCards.find((card) => card.id === id);
-//       if (checkedCard.isChecked) {
-//         setDoneCards((prevDoneCards) => [...prevDoneCards, checkedCard]);
-//         return updatedCards.filter((card) => card.id !== id);
-//       }
-//       return updatedCards;
-//     });
-//   };
-
-//   const handleBodyChange = (value, cardId) => {
-
-//     const updateCards = cards.map((card) => {
-//         if (card.id === cardId) {
-//           return {
-//             ...card,
-//             body: value,
-//           };
-//         }
-//         return card;
-//       });
-
-//       setCards(updateCards);
-//   };
-//   const handleTitleChange = (value, cardId) => {
-
-//     const updateCards = cards.map((card) => {
-//       if (card.id === cardId) {
-//         return {
-//           ...card,
-//           title: value,
-//         };
-//       }
-//       return card;
-//     });
-
-//     setCards(updateCards);
-//   };
-
-//   return (
-//     <div
-//       className="dashboard-container"
-//       style={{ backgroundColor: "rgb(245,245,245)" }}
-//     >
-//       <div className="search-container">{/* ... */}</div>
-
-//       <div
-//         className="dashboard card text-center"
-//         style={{ height: "90%", overflowY: "auto", overflowX: "hidden" }}
-//       >
-//         <div className="card-header">
-//           <ul className="nav nav-pills card-header-pills">
-//             <li className="nav-item">
-//               <a className="nav-link active" href="#">
-//                 To-do
-//               </a>
-//             </li>
-//             <li className="nav-item">
-//               <a className="nav-link" href="#">
-//                 Done
-//               </a>
-//             </li>
-//           </ul>
-//         </div>
-//         <div className="row row-cols-1 row-cols-md-3 g-4">
-//           {cards.map((card) => (
-//             <Card
-//               handleBodyChange={handleBodyChange}
-//               handleCheckboxChange={handleCheckboxChange}
-//               handleClick={handleClick}
-//               handleTitleChange={handleTitleChange}
-//               card={card}
-//               key={card.id}
-//             />
-//           ))}
-//         </div>
-//       </div>
-
-//       <div className="add" onClick={handleAddCard}>
-//         <a href="#" className="add-link">
-//           <span className="add-icon">+</span>
-//         </a>
-//       </div>
-//     </div>
-//   );
-// }
 
 export default function Dashboard() {
+  const [cards, updateCard] = useState([]);
+  const [title, updateTitle] = useState("Card Title");
+  const [subtitle, updateSub] = useState("Card Subtitle");
+  const [innerText, updateInnerText] = useState("Card InnerTitle");
+  let note = [];
+
+  console.log(cards);
+
+  const handleClick = () => {
+    const card = {
+      id: cards.length + 1,
+      title: title,
+      subtitle: "card subtitle",
+      p: "quick text",
+    };
+
+    updateCard([...cards, card]);
+  };
+
+  const handleTitleChange = (e) => {
+    updateTitle(e.target.value);
+  }
+  const handleSubChange = (e) => {
+    updateSub(e.target.value);
+  }
+  const handleTextChange = (e) => {
+    updateInnerText(e.target.value);
+  }
+  const handleKeyDown = (e) => {
+    
+    if (e.key === "Enter") {
+      note.push(cards);
+      console.log("enter pressed")
+     }
+  }
+      console.log(note)
+
   return (
     <div className="dashboard-container">
       <div className="search-bar">
@@ -152,9 +45,51 @@ export default function Dashboard() {
       </div>
       <div className="list-container">
         <div className="list">
-          <div className="card">
-
-          </div>
+          {cards.map((card) => {
+            return (
+              <div
+                className="card bg-primary custom-card"
+                style={{ width: "30%", height: "20%", margin: "1%" }}
+              >
+                <div className="delete-card">
+                  <img
+                    src="/delete-card.svg"
+                    style={{ width: "20px", height: "auto" }}
+                    alt=""
+                  />
+                </div>
+                <div className="card-body">
+                  <input
+                    className="card-title custom-title"
+                    type="text"
+                    placeholder={card.title}
+                    max-length="10"
+                    onChange={handleTitleChange}
+                    onKeyDown={handleKeyDown}
+                    value={title}
+                  />
+                  <input
+                    className="card-subtitle mb-2 text-body-secondary custom-card-subtitle"
+                    type="text"
+                    placeholder={card.subtitle}
+                    onChange={handleSubChange}
+                    value={subtitle}
+                    max-length="10"
+                  />
+                  <input
+                    className="card-text custom-card-text"
+                    type="text"
+                    placeholder={card.p}
+                    onChange={handleTextChange}
+                    value={innerText}
+                  />
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <div className="add" onClick={handleClick}>
+          <span className="add-icon">+</span>
         </div>
       </div>
     </div>
