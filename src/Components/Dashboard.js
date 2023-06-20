2a5f425d461921532ae46d01cf61b7e6ce74b0ce
molokochris <molokochrisp742@gmail.com>
2023-06-20 10:13:37 +0200

    fix modal

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,26 +1,30 @@
 import { useState } from "react";
+import { Button, Modal, Dropdown } from "react-bootstrap";
 
 export default function Dashboard() {
   const [cards, updateCard] = useState([]);
-  const [title, updateTitle] = useState("Card Title");
-  const [subtitle, updateSub] = useState("Card Subtitle");
-  const [innerText, updateInnerText] = useState("Card InnerTitle");
-  let note = [];
-
-  console.log(cards);
+  const [title, updateTitle] = useState("");
+  const [subtitle, updateSub] = useState("");
+  const [innerText, updateInnerText] = useState("");
+  const [cardColor, updateCardColor] = useState("");
 
   const handleClick = () => {
     const card = {
       id: cards.length + 1,
       title: title,
-      subtitle: "card subtitle",
-      p: "quick text",
+      subtitle: subtitle,
+      p: innerText,
+      color: cardColor,
     };
 
     updateCard([...cards, card]);
+    handleClose();
   };
 
-  const handleDelete = (e) => {};
+  const handleDelete = (e) => {
+    const updatedCards = cards.filter((card) => card.id != e);
+    updateCard(updatedCards)
+  };
 
   const handleTitleChange = (e) => {
     updateTitle(e.target.value);
@@ -31,12 +35,15 @@ export default function Dashboard() {
   const handleTextChange = (e) => {
     updateInnerText(e.target.value);
   };
-  const handleKeyDown = (e) => {
-    if (e.key === "Enter") {
-      note.push(cards);
-      console.log("enter pressed");
-    }
+  const handleColorChange = (e) => {
+    // updateCardColor(e.target.value);
+    updateCardColor(e)
+    console.log(e)
   };
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
     <div className="dashboard-container">
@@ -48,10 +55,10 @@ export default function Dashboard() {
           {cards.map((card) => {
             return (
               <div
-                className="card bg-primary custom-card"
+                className={`${card.color} card custom-card`}
                 style={{ width: "30%", height: "20%", margin: "1%" }}
               >
-                <div className="delete-card" onClick={handleDelete}>
+                <div className="delete-card" onClick={() => handleDelete(card.id)}>
                   <img
                     src="/delete-card.svg"
                     style={{ width: "20px", height: "auto" }}
@@ -65,15 +72,14 @@ export default function Dashboard() {
                     placeholder={card.title}
                     max-length="10"
                     onChange={handleTitleChange}
-                    onKeyDown={handleKeyDown}
-                    value={title}
+                    value={card.title}
                   />
                   <input
                     className="card-subtitle mb-2 text-body-secondary custom-card-subtitle"
                     type="text"
                     placeholder={card.subtitle}
                     onChange={handleSubChange}
-                    value={subtitle}
+                    value={card.subtitle}
                     max-length="10"
                   />
                   <input
@@ -88,46 +94,71 @@ export default function Dashboard() {
             );
           })}
         </div>
-        <button className="add" data-bs-toggle="modal" data-bs-target="#exampleModal">
-          {/* <span className="add-icon">+</span> */}launch
-        </button>
-        {/* <!-- Modal -- */}
-        <div
-          class="modal fade"
-          id="exampleModal"
-          tabindex="-1"
-          aria-labelledby="exampleModalLabel"
-          aria-hidden="true"
-        >
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">
-                  Modal title
-                </h1>
-                <button
-                  type="button"
-                  class="btn-close"
-                  data-bs-dismiss="modal"
-                  aria-label="Close"
-                ></button>
-              </div>
-              <div class="modal-body">...</div>
-              <div class="modal-footer">
-                <button
-                  type="button"
-                  class="btn btn-secondary"
-                  data-bs-dismiss="modal"
-                >
-                  Close
-                </button>
-                <button type="button" class="btn btn-primary">
-                  Save changes
-                </button>
-              </div>
-            </div>
-          </div>
+        <div className="add" variant="primary" onClick={handleShow}>
+          <span className="add-icon">+</span>
         </div>
+        <Modal
+          show={show}
+          onHide={handleClose}
+          backdrop="static"
+          keyboard={false}
+          centered
+          size="lg"
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Add Task</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <input
+              className="card-title custom-title"
+              type="text"
+              placeholder=" Enter To-do Item "
+              max-length="10"
+              onChange={handleTitleChange}
+              value={title}
+            />
+            <input
+              className="card-subtitle mb-2 text-body-secondary custom-card-subtitle"
+              type="text"
+              placeholder="Enter Subtext"
+              onChange={handleSubChange}
+              value={subtitle}
+              max-length="10"
+            />
+            <input
+              className="card-text custom-card-text"
+              type="text"
+              placeholder="Enter More Info"
+              onChange={handleTextChange}
+              value={innerText}
+            />
+            <Dropdown className="d-flex justify-content-center">
+              <Dropdown.Toggle variant="success" id="dropdown-basic">
+                Choose Priority
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item value={cardColor} onClick={() => handleColorChange("bg-danger")}>
+                  High
+                </Dropdown.Item>
+                <Dropdown.Item
+                   value={cardColor}
+                  onClick={() => handleColorChange("bg-primary")}
+                >
+                  Mid
+                </Dropdown.Item>
+                <Dropdown.Item value={cardColor} onClick={() => handleColorChange("bg-secondary")}>
+                  Low
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={handleClick}>
+              Add
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     </div>
   );
