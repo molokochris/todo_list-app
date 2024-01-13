eaad5b95e49938838b76fcde4abb1b717fdd11cb
molokochris <molokochrisp742@gmail.com>
2024-01-13 18:53:27 +0200

    todo list logic

--- src/Components/Checkbox.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Checkbox = ({ onChange }) => {
-  return (
-    <input onChange={onChange} type={"checkbox"} className="border border-teal-700 outline-none    my-1"/>
-  );
-};
-
-export default Checkbox;
--- src/axios/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export const myAxios = () => {
-  return axios.create({
-    baseURL: "http://localhost:3000",
-  });
-};
